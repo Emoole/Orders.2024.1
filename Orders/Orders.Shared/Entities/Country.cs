947abf066a19ea829e093e6387e8a9f80c57ded9
Emoole <emonlop@outlook.com>
2024-04-30 14:12:56 -0500

    Create DataBase with the first entity country.

---
+++ Orders/Orders.Shared/Entities/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Pais")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string Name { get; set; } = null!;
+    }
+}
